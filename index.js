8025a3e4fe1a667c4405a38a50e6b01be205914a
Thanatos <sunn@gmx.com>
2019-09-22 20:44:01 +0200

    4rd commit

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+var server = require('http').createServer(app);
+
+app.get("/", (request, response) => {
+    console.log(Date.now() + " Ping received");
+    response.sendStatus(200);
+});
+const listener = server.listen(process.env.PORT, function() {
+    console.log('Your app is listening on port ' + listener.adress().port);
+});
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+
+
+
 
 //const TOKEN = process.env.TOKEN;
 
