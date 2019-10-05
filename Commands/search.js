d893f52de911e5946ff66cf9ff099f4f46a3c1b0
Thanatos <sunn@gmx.com>
2019-10-05 19:24:10 +0200

    objection

--- Commands/search.js
+++ Commands/search.js
@@ -1,16 +1,28 @@
 const Discord = require('discord.js');
-const superagent = require('superagent');
 
 module.exports.run = async (client, message, args) => {
+    const rando_imgs = [
+        './objectionImage/objection1.gif',
+        './objectionImage/objection2.jpg',
+        './objectionImage/objection3.png',
+        './objectionImage/objection4.gif',
+        './objectionImage/objection5.jpg',
+        './objectionImage/objection6.jpg',
+        './objectionImage/objection7.gif',
+        './objectionImage/objection8.gif',
+        './objectionImage/objection9.gif',
+        './objectionImage/objection10.gif',
+        './objectionImage/objection11.gif',
+        './objectionImage/objection12.gif',
+        './objectionImage/objection13.jpg'
+    ];
 
-    superagent.get('https://nekobot.xyz/api/image')
-    .query({type: 'animeface'})
-    .end((err, response) => {
-        msg.channel.send({file: response.body.message});
+    message.channel.send({
+        file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
     });
 };
 
 
 module.exports.help = {
-    name:"anime"
+    name:"objection"
 };
