533a1f94d2699ba7d7c4fe646ea4d0ba4786469b
Thanatos <sunn@gmx.com>
2019-10-05 18:00:41 +0200

    all

--- Commands/search.js
+++ Commands/search.js
@@ -1,36 +1,40 @@
 const Discord = require('discord.js');
+const request = require('request');
+const cheerio = require('cheerio');
 
 module.exports.run = async(client, message, args, ) => {
     var parts = message.content.split(" ");
 
-    var search = parts.slice(1).join(" ");
+    function image(message, parts) {
+        var search = parts.slice(1).join(" ");
 
-    var options = {
-        url: "https://www.metacrawler.com/serp?q=" + search,
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    };
-    request(options, function (error, response, responsesBody) {
-        if (error) {
-            return;
-        }
+        var options = {
+            url: "https://www.metacrawler.com/serp?q=" + search,
+            method: "GET",
+            headers: {
+                "Accept": "text/html",
+                "User-Agent": "Chrome"
+            }
+        };
+        request(options, function (error, response, responsesBody) {
+            if (error) {
+                return;
+            }
 
-        $ = cheerio.load(responsesBody);
-        var links = $(".image a.link");
+            $ = cheerio.load(responsesBody);
+            var links = $(".image a.link");
 
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+            var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
 
-        if (!urls.length) {
-            return;
-        }
+            if (!urls.length) {
+                return;
+            }
 
-        message.channel.send(urls[~~(Math.random()*5)]);
-    });
-
-    module.exports.help = {
-        name:"image"
+            message.channel.send(urls[~~(Math.random()*5)]);
+        });
     }
+};
+
+module.exports.help = {
+    name:"image"
 };
