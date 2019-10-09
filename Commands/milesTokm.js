352270a65a7b9b21a48f60df2cf54e56dc1e09d5
Thanatos <sunn@gmx.com>
2019-10-09 23:46:20 +0200

    urban

--- Commands/milesTokm.js
+++ Commands/milesTokm.js
@@ -12,6 +12,6 @@ module.exports.run = async (client, message, args) => {
 };
 
 module.exports.help = {
-    name: 'kmTomiles'
+    name: 'milesTokm'
 };
 
--- Commands/urban.js
+++ Commands/urban.js
@@ -7,6 +7,6 @@ module.exports.run = async (client, message, args) => {
 };
 
 module.exports.help = {
-    name: 'kmTomiles'
+    name: 'def'
 };
 
