eb59d086b63abe974cd105e43fe13dc3bc723e78
Thanatos <sunn@gmx.com>
2019-10-09 23:49:25 +0200

    urban

--- Commands/urban.js
+++ Commands/urban.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js');
+const urban = require('urban');
 
 module.exports.run = async (client, message, args) => {
     var def = urban(args[0]);
+
+    def.first(function (json) {
+        console.log(json);
+    });
     message.channel.startTyping();
+
     message.channel.send(`la definition du mot ${def} est: ***${def}`)
+
+    message.channel.stopTyping();
 };
 
 module.exports.help = {
