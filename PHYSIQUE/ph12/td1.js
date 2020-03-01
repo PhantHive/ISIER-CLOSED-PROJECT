c3efb841ea0cce5e009b24cc9661b3ebb53d2e73
PhantHive <phantomhive72@yahoo.fr>
2020-03-01 22:58:02 +0100

    yes

--- PHYSIQUE/ph12/td1.js
+++ PHYSIQUE/ph12/td1.js
@@ -10,15 +10,15 @@ module.exports = async(client, message) => {
 	
 	var aideTrim = aide.toUpperCase().replace(/\s/g,'')
 	
-	if (guild.channel.id === '') {
+	if (message.channel.id === '613749495716642818') {
 		
 		if (aideTrim.indexOf("ex 4 td1 " || "exercice td1 " || "exo 4 du td1" || "exo 4") ) {
 			client.startTyping(message.channel)
-			message.channel.send("Hello! tu as apparement besoin d'aide sur l'exercice 4 du td 1 de ph12, est-ce bien ca?").then(
+			message.channel.send("Hello! tu as apparement besoin d'aide sur l'exercice 4 du td 1 de ph12, est-ce bien ca?")
 		
-		if (message.content === "oui") {
-			message.reply("test")
-		})
+		  if (message.content === "oui") {
+			  message.reply("test")
+		  }
 			
 		}
 		
