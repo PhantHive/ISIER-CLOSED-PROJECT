852ef8637c26e5ece41c58e42e07d5c992fa6ba4
PhantHive <phantomhive72@yahoo.fr>
2020-03-01 23:36:07 +0100

    yes

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ fs.readdir("./ph12/", (err, f) => {
     console.log(`${f.length} td loading`);
 
     f.forEach((f) => {
-        const physiques = require(`./PHYSIQUE/ph12/${f}`);
+        const physiques = require(`./ph12/${f}`);
         const physique = f.split(".")[0];
         client.on(physique, physiques.bind(null, client));
     });
