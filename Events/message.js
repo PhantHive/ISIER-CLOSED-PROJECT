3b5bec3b6d828b9b470bf150827f35062d8b440c
Thanatos <sunn@gmx.com>
2020-03-28 18:12:24 +0100

    e

--- Events/message.js
+++ Events/message.js
@@ -1,75 +1,18 @@
 const Discord = require('discord.js');
 const prefix = "i!";
-const fs = require("fs")
+const fs = require("fs");
 
 
 module.exports = async(client, message) => {
 
-    if (message.author.bot) return;
+    //=============
 
-    //fonction special
-    var chaine = String(message.content);
 
-    if (chaine.indexOf("dropbox") !== -1) {
-        message.reply('Go ici amigo=> <#611829345446658048>' );
-    }
-
-    var chaine2 = String(message.content);
-
-    if (chaine2.indexOf("chut") !== -1) {
-        message.channel.send('ce manque de respect popopopo j\'aurai pas aimer');
-    }
-
-    var chaine3 = String(message.content);
-
-    if (chaine3.indexOf("bonsoir") !== -1) {
-        message.channel.send('salut mon ami ;)');
-    }
-
-    var chaine4 = String(message.content);
-
-    if (chaine4.indexOf("t'as fait") !== -1) {
-        message.channel.send('nop pas encore deso');
-    }
-
-    var chaine5 = String(message.content);
-
-    if (chaine5.indexOf("???") !== -1) {
-        message.channel.send('ceci etait une question que qql y reponde please (*ouai tu peux m\'appeler Dieu gamin*)');
-    }
-
-    var chaine6 = String(message.content);
-
-    if (chaine6.indexOf("il est gentil") !== -1) {
-        message.reply('je te promet c est vrai tavu');
-    }
-
-    var chaine7 = String(message.content);
-
-    if (chaine7.indexOf("mdr") !== -1) {
-        message.react('😂');
-    }
     //=================
 
 
-    
+
     //==============================
-
-
-
-    if (message.channel.type === "dm") {
-
-      if(message.content === "je t'aime") {
-
-        message.reply("***Tu es mon premier amour!***")
-
-      } else {
-
-        message.reply("Utilises le channel discord commandes bot pour pouvoir m'utiliser")
-
-      }
-    }
-
 
     if (!message.content.startsWith(prefix)) return;
 
@@ -81,90 +24,6 @@ module.exports = async(client, message) => {
     if(!cmd) return message.reply("il semblerai que tu ai besoin de m'appeler, si tu ne connais pas les commandes je t'invite a faire i!aide");
 
     cmd.run(client, message, args);
-
-
-
-  //xp system
-
-
-
-/*
-  const infoLvl = require('../jsonFile/xp.json');
-
-  let xpAdd = Math.floor(Math.random()*7) + 1;
-
-  if(!infoLvl[message.author.id]) { //si l'utilisateur n'a pas de profil niveau, on lui etablit un
-    infoLvl[message.author.id] = {
-
-        xp: 0,
-        lvl: 1
-
-    };
-  }
-
-
-  //=================
-  var curxp = infoLvl[message.author.id].xp; //servira pour afficher les xp
-
-  var curLvl = infoLvl[message.author.id].lvl; //servira pour afficher les levels
-
-  infoLvl[message.author.id].xp = curxp + xpAdd;
-
-  // ===============
-
-
-
-    let nxtLvl = infoLvl[message.author.id].lvl * 500 ; //on definit le premier level a gagner a 500xp (on passe niveau i + 1)
-
-
-    if (nxtLvl <= infoLvl[message.author.id].xp && curLvl < 10) {
-
-      infoLvl[message.author.id].lvl = curLvl + 1;
-
-      let lvlup = new Discord.RichEmbed()
-            .setTitle("LVL +")
-            .setColor("GREEN")
-            .addField(message.author.username + " niveau atteint: ", curLvl + 1, true)
-            .addField("XP: ", curxp)
-            .setImage("https://i.imgur.com/FFYT8Ll.png");
-        message.guild.channels.get('502932200975630336').send(lvlup);
-
-
-    } else if (nxtLvl <= infoLvl[message.author.id].xp && curLvl < 25) {
-
-      infoLvl[message.author.id].lvl = curLvl + 1;
-
-      let lvlup2 = new Discord.RichEmbed()
-            .setTitle("LVL ++")
-            .setColor("BLUE")
-            .addField(message.author.username + " TU ES BON, niveau atteint: ", curLvl + 1, true)
-            .addField("XP: ", curxp)
-            .setImage("https://i.imgur.com/7LVMSKN.png");
-        message.guild.channels.get('502932200975630336').send(lvlup2);//622155677338566656
-
-    } else if (nxtLvl <= infoLvl[message.author.id].xp && curLvl < 50) {
-
-      infoLvl[message.author.id].lvl = curLvl + 1;
-
-      let lvlup3 = new Discord.RichEmbed()
-            .setTitle("LVL +++++")
-            .setColor("ORANGE")
-            .addField(message.author.username + " is GOD LIKE, niveau atteint: ", curLvl + 1, true)
-            .addField("XP: ", curxp)
-            .setImage("https://i.imgur.com/Mnx9Vu0.jpg");
-        message.guild.channels.get('502932200975630336').send(lvlup3)
-
-    } else if (nxtLvl === infoLvl[message.author.id].xp && curLvl === 50) {
-
-      message.channels.get('502932200975630336').send("WTF MEN TU ES CHAUD!")
-
-    }
-    fs.writeFile("./json/infolvl.json", JSON.stringify(infoLvl), (err) => {
-        if (err) console.log(err)
-    });
-
-*/
-  // puis on multiplie par le niveau i pour augmenter le niveau d xp a avoir proportionnelement
 
 
 
--- index.js
+++ index.js
@@ -12,7 +12,8 @@ var server = require('http').createServer(app);
 
 //XP PART
 let infoLVL = require("./jsonFile/level.json");
-
+//easter part
+let easterEgg = require("./jsonFile/easterEgg.json")
 
 //=========
 app.get("/", (request, response) => {
@@ -94,60 +95,118 @@ client.on('guildMemberAdd', member => {
 
 client.on("message", async message => {
 
-      let gainXP = Math.floor(Math.random() * 5) + 1; //add some xp by message
-      console.log(gainXP)
+    let gainXP = Math.floor(Math.random() * 5) + 10; //add some xp by message
+    console.log(gainXP)
       //we look for infoLVL of the message author on the json file if not defined then we define it
-      if (!infoLVL[message.author.id]){
+    if (!infoLVL[message.author.id]){
         infoLVL[message.author.id] = {
           xp: 0,
           lvl: 1
         };
-      }
+    }
 
       //data
-      let curxp = infoLVL[message.author.id].xp;
-      let curlvl = infoLVL[message.author.id].lvl;
-      let newlvl = curlvl*10;
+    let curxp = infoLVL[message.author.id].xp;
+    let curlvl = infoLVL[message.author.id].lvl;
+    let newlvl = 5 * (curlvl ** 2) + 69 * curlvl + 249;
       //data
 
       //auto accumulate xp
-      infoLVL[message.author.id].xp = curxp + gainXP;
+    infoLVL[message.author.id].xp = curxp + gainXP;
 
       //look for a new rank master
-      if (curlvl < 5) {
 
-        if (newlvl <= infoLVL[message.author.id].xp) {
-          infoLVL[message.author.id].lvl = curlvl + 1;
-          let lvlup = new Discord.RichEmbed()
-                .setTitle("LVL +")
-                .setColor("GREEN")
-                .addField(message.author.username + " niveau atteint: ", curlvl + 1, true)
-                .addField("XP: ", curxp)
-                .setImage("https://i.imgur.com/FFYT8Ll.png");
-            message.guild.channels.get('502931781012684820').send(lvlup);
-        }
+    if (message.author.bot) {
+        return;
+    }
 
-      }
-      else if (curlvl < 10) {
+    if (newlvl <= infoLVL[message.author.id].xp && curlvl < 5) {
+        infoLVL[message.author.id].lvl = curlvl + 1;
 
-        if (newlvl <= infoLVL[message.author.id].xp) {
-          infoLVL[message.author.id].lvl = curlvl + 1;
-          let lvlup2 = new Discord.RichEmbed()
-                .setTitle("LVL ++")
-                .setColor("BLUE")
-                .addField(message.author.username + " TU ES BON, niveau atteint: ", curlvl + 1, true)
-                .addField("XP: ", curxp)
-                .setImage("https://i.imgur.com/7LVMSKN.png");
-            message.guild.channels.get('502931781012684820').send(lvlup2);
-        }
+        let lvlup = new Discord.RichEmbed()
+            .setTitle("LVL +")
+            .setColor("GREEN")
+            .addField(message.author.username + " niveau atteint: ", curlvl + 1, true)
+            .addField("XP: ", newlvl)
+            .setImage("https://i.imgur.com/FFYT8Ll.png");
+        message.guild.channels.get('502931781012684820').send(lvlup);
 
-      }
-      fs.writeFile("./jsonFile/level.json", JSON.stringify(infoLVL), (err) => {
-        if (err) console.log(err);
-      });
+        infoLVL[message.author.id].xp = 0;
+    }
+    else if (newlvl <= infoLVL[message.author.id].xp && curlvl < 10) {
+
+        infoLVL[message.author.id].lvl = curlvl + 1;
+        let lvlup2 = new Discord.RichEmbed()
+            .setTitle("LVL ++")
+            .setColor("BLUE")
+            .addField(message.author.username + " TU ES BON, niveau atteint: ", curlvl + 1, true)
+            .addField("XP: ", newlvl)
+            .setImage("https://i.imgur.com/7LVMSKN.png");
+        message.guild.channels.get('502931781012684820').send(lvlup2);
+
+        infoLVL[message.author.id].xp = 0;
+    }
+    else if (newlvl <= infoLVL[message.author.id].xp && curlvl < 15) {
+
+        infoLVL[message.author.id].lvl = curlvl + 1;
+		let lvlup3 = new Discord.RichEmbed()
+            .setTitle("LVL +++++")
+            .setColor("ORANGE")
+            .addField(message.author.username + " is GOD LIKE, niveau atteint: ", curlvl + 1, true)
+            .addField("XP: ", newlvl)
+            .setImage("https://i.imgur.com/Mnx9Vu0.jpg");
+        message.guild.channels.get('502931781012684820').send(lvlup3)
+
+        infoLVL[message.author.id].xp = 0;
+	}
+    else {
+        infoLVL[message.author.id].lvl;
+        infoLVL[message.author.id].xp;
+    }
+    fs.writeFile("./jsonFile/level.json", JSON.stringify(infoLVL), (err) => {
+          if (err) console.log(err);
+          });
 
 
       console.log(`ur level: ${curlvl}`);
+
+     //Easter egg system counter
+
+    if (!easterEgg[message.author.id]) {
+      easterEgg[message.author.id] = {
+            counter: 0
+        }
+    }
+
+    let curCounter = easterEgg[message.author.id].counter;
+
+
+    if (message.channel.type === "dm") {
+
+        if(message.content === "je t'aime") {
+            console.log(infoLVL)
+            if (curCounter == 0) {
+
+                message.reply("***Tu es mon premier amour!*** Tu as complete un easterEgg, tu ne peux plus gagner de point la dessus, tu as gagne 500xp");
+                easterEgg[message.author.id].counter = curCounter + 1;
+                infoLVL[message.author.id].lvl = curlvl + 2;
+                console.log(easterEgg)
+                console.log(infoLVL)
+            } else {
+              message.reply("Utilises le channel discord commandes bot pour pouvoir m'utiliser")
+            }
+            fs.writeFile("./jsonFile/easterEgg.json", JSON.stringify(easterEgg), (err) => {
+            if (err) console.log(err);
+            });
+
+
+
+      } else {
+
+        message.reply("Utilises le channel discord commandes bot pour pouvoir m'utiliser")
+
+      }
+    }
 
 
 });
