a2190340d2de2bfb45ea21d6b74156c59d8c9a85
Thanatos <sunn@gmx.com>
2020-07-21 15:28:38 +0200

    test

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
-const TOKEN = process.env.TOKEN;
+const TOKEN = process.env.token;
 
 client.login(TOKEN);
 client.commands = new Discord.Collection();
