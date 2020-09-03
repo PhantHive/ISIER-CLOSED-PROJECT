95479d05985a8883f26db3d4e7acdb1d375f8197
Thanatos <sunn@gmx.com>
2020-09-03 23:56:47 +0200

    pyaide

--- Commands/fonction.js
+++ Commands/fonction.js
@@ -4,15 +4,15 @@
 module.exports.run = async(client, message, args) => {
 
     message.channel.send("Une fonction, est dans tous langages de programmation un moyen d'exécuter une tache autant de fois que n'ont le souhaitent pour une,\n " + 
-                        "plusieurs ou aucune valeur.\n " +
-                        "Syntaxe python:\n" +
+                        "plusieurs ou aucune valeur.\n\n " +
+                        "Syntaxe python:\n\n" +
                         "def fonction():\n" +
                         "   #ecriture de la fonction\n" +
                         "Deux exemples: \n\n")
                         
-                        .then( () => message.channel.send({file: "./python/Hello.png"}))
+                        .then( () => message.channel.send({file: "./python/Hello.PNG/"}))
                         .then( () => message.channel.send("Exemple 2: \n"))
-                        .then( () => message.channel.send({file: "./python/helloWorld.png"}))
+                        .then( () => message.channel.send({file: "./python/helloWorld.PNG/"}))
 
 
 };
