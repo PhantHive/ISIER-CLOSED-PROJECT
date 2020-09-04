20b64a34c44caf700857077d05372403235c11c9
Thanatos <sunn@gmx.com>
2020-09-04 02:40:11 +0200

    addBase facile diff et hard

--- Commands/poll.js
+++ Commands/poll.js
@@ -58,7 +58,7 @@ module.exports.run = async ( client, message, args) => {
                                 message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 30000}).then(collected =>
                                     {
                                         if (collected.first().content == res) {
-                                            message.reply("BRAVO!, Tu re-veux une addition?").then( () => {
+                                            message.reply("BRAVO!, Tu re-veux une addition? (oui/non)").then( () => {
                                                 message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 10000}).then(collected => {
                                                     if (collected.first().content == "oui") {
                                                         a = Math.floor((Math.random()* 2) + 0);
@@ -124,7 +124,7 @@ module.exports.run = async ( client, message, args) => {
                                 message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 30000}).then(collected =>
                                     {
                                         if (collected.first().content == res) {
-                                            message.reply("BRAVO!, Tu re-veux une addition?").then( () => {
+                                            message.reply("BRAVO!, Tu re-veux une addition? (oui/non)").then( () => {
                                                 message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 10000}).then(collected => {
                                                     if (collected.first().content == "oui") {
                                                         a = Math.floor((Math.random()* 2) + 0);
@@ -194,7 +194,7 @@ module.exports.run = async ( client, message, args) => {
                                 message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 30000}).then(collected =>
                                     {
                                         if (collected.first().content == res) {
-                                            message.reply("BRAVO!, Tu re-veux une addition?").then( () => {
+                                            message.reply("BRAVO!, Tu re-veux une addition? (oui/non)").then( () => {
                                                 message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 10000}).then(collected => {
                                                     if (collected.first().content == "oui") {
                                                         a = Math.floor((Math.random()* 2) + 0);
@@ -249,7 +249,91 @@ module.exports.run = async ( client, message, args) => {
 
 
             //COLLECT HEXA
+            hexa.on('collect', r => {
+                const embedChoice = new Discord.RichEmbed()
+                    .setTitle("PROGRAMME BINAIRE")
+                    .setColor("ORANGE")
+                    .addField("Choisissez votre niveau de difficulte:")
+                    .setFooter("Reagissez avec l'une des emojis ci-dessous");
+                message.channel.send(embedChoice).then(msg =>
+                    msg.react('🙂').then(r => {
+                        msg.react('😖')
+                        msg.react('😤');
+                        //FILTRES
+                        const easyFilter = (reaction, user) => reaction.emoji.name == '🙂' && user.id === message.author.id
+                        const mediumFilter = (reaction, user) => reaction.emoji.name == '😖' && user.id === message.author.id
+                        const hardFilter = (reaction, user) => reaction.emoji.name == '😤' && user.id === message.author.id
 
+                        const easy = msg.createReactionCollector(easyFilter, {time: 15000});
+                        const medium = msg.createReactionCollector(mediumFilter, {time: 15000});
+                        const hard = msg.createReactionCollector(hardFilter, {time: 15000});
+
+                        easy.on('collect', r => {
+
+                            var hexadecimal = Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f")
+                            a = hexadecimal[Math.floor(Math.random()*hexadecimal.length)];
+                            b = hexadecimal[Math.floor(Math.random()*hexadecimal.length)];
+                            c = hexadecimal[Math.floor(Math.random()*hexadecimal.length)];
+
+                            aa = hexadecimal[Math.floor(Math.random()*hexadecimal.length)]
+                            bb = hexadecimal[Math.floor(Math.random()*hexadecimal.length)]
+                            cc = hexadecimal[Math.floor(Math.random()*hexadecimal.length)]
+
+                            firstNumb = "0x" + a.toString() + b.toString() + c.toString()
+                            secNumb = "0x" + aa.toString() + bb.toString() + cc.toString()
+                            
+                
+                            res = (parseInt(firstNumb, 16) + parseInt(secNumb, 16)).toString(16); //solution
+
+                            message.channel.send(`Faites l'operation suivante: ${firstNumb} + ${secNumb}`)
+                            .then( () => {
+
+                                message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 30000}).then(collected =>
+                                    {
+                                        if (collected.first().content == res) {
+                                            message.reply("BRAVO!, Tu re-veux une addition? (oui/non)").then( () => {
+                                                message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 10000}).then(collected => {
+                                                    if (collected.first().content == "oui") {
+                                                        var hexadecimal = Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f")
+                                                        a = hexadecimal[Math.floor(Math.random()*hexadecimal.length)];
+                                                        b = hexadecimal[Math.floor(Math.random()*hexadecimal.length)];
+                                                        c = hexadecimal[Math.floor(Math.random()*hexadecimal.length)];
+                            
+                                                        aa = hexadecimal[Math.floor(Math.random()*hexadecimal.length)]
+                                                        bb = hexadecimal[Math.floor(Math.random()*hexadecimal.length)]
+                                                        cc = hexadecimal[Math.floor(Math.random()*hexadecimal.length)]
+                            
+                                                        firstNumb = "0x" + a.toString() + b.toString() + c.toString()
+                                                        secNumb = "0x" + aa.toString() + bb.toString() + cc.toString()
+                                                        
+                                            
+                                                        res = (parseInt(firstNumb, 16) + parseInt(secNumb, 16)).toString(16); //solution
+
+                                                        message.channel.send(`Faites l'operation suivante: ${firstNumb} + ${secNumb}`)
+                                                        .then ( () => {
+                                                            message.channel.awaitMessages(msg => msg.author.id == message.author.id, {max: 1, time: 10000}).then(collected => {
+                                                                if (collected.first().content == res) {
+                                                                    message.reply("Bravo, relance le programme de calcul avec i!addBase, i!subBase si besoin")
+                                                                }
+                                                                else {
+                                                                    message.reply(`Dommage la reponse était: ${res} relance le programme de calcul avec i!addBase, i!subBase si besoin`)
+                                                                }
+                                                            })
+                                                        })
+                                                    }
+                                                })
+                                            })
+                                        }
+                                        else {
+                                            message.reply(`Dommage la reponse était: ${res}`)
+                                        }
+                                    });
+                               
+                            
+                            });
+                        
+                        })
+            })
         })
     )
 };
