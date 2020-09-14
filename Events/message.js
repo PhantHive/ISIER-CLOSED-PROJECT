2e0c33e0ea05fb281d66964f03e7074fd1ec4999
Thanatos <sunn@gmx.com>
2020-09-14 23:20:22 +0200

    2 discord a gerer

--- Events/message.js
+++ Events/message.js
@@ -32,7 +32,7 @@ module.exports = async(client, message) => {
     var chaine = String(message.content);
 
     if (chaine.indexOf("dropbox") !== -1) {
-        const dropboxChannel = message.guild.find(channel => channel.name === "🎓▶liens-et-ressources-utiles◀🎓");
+        const dropboxChannel = client.guild.channels.find(channel => channel.name === "🎓▶liens-et-ressources-utiles◀🎓");
         message.reply(`Go ici amigo=> ${dropboxChannel}` );
     }
 
