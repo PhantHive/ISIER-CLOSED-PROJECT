01fbbd7b6ff15b45954a4b95c7d63a06f3fdce67
Thanatos <sunn@gmx.com>
2020-09-21 23:21:44 +0200

    no sql xp level system

--- Commands/rang.js
+++ Commands/rang.js
@@ -29,28 +29,30 @@ module.exports.run = async (client, message,args) => {
               LEVEL: 1
           });
           newD.save();
-    } 
+    } else {
+      let curxp = data.XP;
+    let curlvl = data.LEVEL;
+    let newlvl = 5 * (curlvl** 2) + 69 * curlvl + 249;
+
+    /*
+    let curxp = infoLVL[message.author.id].xp;
+    let curlvl = infoLVL[message.author.id].lvl;
+    let newlvl = 5 * (curlvl** 2) + 69 * curlvl + 249;
+    */
+    let rang = new Discord.RichEmbed() 
+      .setAuthor(message.author.username)
+      .setColor("black")
+      .addField("XP: ", curxp, true)
+      .addField("LEVEL: ", curlvl, true)
+      .addField("PROGRESSION: ", `${curxp}  /  ${newlvl}`, true )
+      .addField("======================", "<systemXP>")
+      .setFooter(", base sur la formule:  5 * (votreniveau ^ 2) + 69 * votreniveau + 249")
+
+    message.channel.send(rang);
+    };
   });
 
-  let curxp = data.XP;
-  let curlvl = data.LEVEL;
-  let newlvl = 5 * (curlvl** 2) + 69 * curlvl + 249;
-
-  /*
-  let curxp = infoLVL[message.author.id].xp;
-  let curlvl = infoLVL[message.author.id].lvl;
-  let newlvl = 5 * (curlvl** 2) + 69 * curlvl + 249;
-  */
-  let rang = new Discord.RichEmbed() 
-    .setAuthor(message.author.username)
-    .setColor("black")
-    .addField("XP: ", curxp, true)
-    .addField("LEVEL: ", curlvl, true)
-    .addField("PROGRESSION: ", `${curxp}  /  ${newlvl}`, true )
-    .addField("======================", "<systemXP>")
-    .setFooter(", base sur la formule:  5 * (votreniveau ^ 2) + 69 * votreniveau + 249")
-
-  message.channel.send(rang);
+  
 };
 
 
