89e86eb44bc3b81130791fb9cf4414712e72d0b3
Thanatos <sunn@gmx.com>
2020-09-22 04:46:37 +0200

    canvas

--- Commands/rang.js
+++ Commands/rang.js
@@ -77,7 +77,7 @@ module.exports.run = async (client, message,args) => {
         ctx.fillText(`${curxp}  /  ${newlvl} XP`, 600, 260);
         ctx.textAlign = "left";
         ctx.font = "bold 45px Comic Sans MS";
-        ctx.shadowOffsetX = h_distance;
+        ctx.shadowOffsetX = 5;
         ctx.shadowColor = "blue";
         ctx.shadowBlur = 3;
         ctx.fillText(member.user.tag, 300, 120);
