99b52cf8e75b341b5bc9e7e5a560e8ddff1ff29f
Thanatos <sunn@gmx.com>
2020-09-22 11:01:59 +0200

    canvas

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ client.on("message", async message => {
     (err, data) => {
         if (err) console.log(err);
 
-        let gainXP = Math.floor(Math.random() * 10) ; //add some xp by message
+        var xpRand = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let gainXP = Math.floor(Math.random() * xpRand.length) ; //add some xp by message
 
         if (message.author.bot) {
             return;
