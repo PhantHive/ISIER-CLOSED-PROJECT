e4208c7c61a8a33990207ef3526f8e9b61bba8d4
Thanatos <sunn@gmx.com>
2020-09-24 00:18:00 +0200

    rank

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on("message", async message => {
     lb.then((doc) => {
         doc.forEach((doc) => {
             if ( doc.name != current || current == null ) {
-                rank = 1;
+                rank = 0;
                 current = doc.name;
             }
             rank++;
