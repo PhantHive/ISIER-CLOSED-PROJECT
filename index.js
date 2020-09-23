2b2210143fc858db8e0a86c4d0c7eaf0a9d5fbb8
Thanatos <sunn@gmx.com>
2020-09-24 01:01:29 +0200

    rank

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on("message", async message => {
     var current = null,
         rank = 0;
 
-    var lb = XLD.find().sort({ "LEVEL": -1, "XP": -1 });
+    var lb = XLD.find({serverID: message.guild.id}).sort({"ID": 1, "LEVEL": -1, "XP": -1 });
     lb.exec((err, doc) => {
         if (err) console.log(err);
 
@@ -72,6 +72,7 @@ client.on("message", async message => {
             }
             rank++;
             doc.RANK = rank;  
+            
             console.log(doc);
         })
         
