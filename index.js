26c979c12e4215bdf48460dab9d667095c90c697
Thanatos <sunn@gmx.com>
2020-09-24 01:17:57 +0200

    rank

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ client.on("message", async message => {
         if (err) console.log(err);
 
         for (const underDoc of doc){
-            for(i=0; underDoc < doc.length; i++) {
+
+            for (let i=0; underDoc < doc.length; i++) {
                 underDoc.RANK = i+1;
                 console.log(underDoc.RANK)
             }
