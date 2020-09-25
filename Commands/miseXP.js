47c2142076f60134719b8c5df49ad2d176c084cb
Thanatos <sunn@gmx.com>
2020-09-25 17:43:45 +0200

    miseXp

--- Commands/miseXP.js
+++ Commands/miseXP.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
 
     const challenger = message.author.id;
     const adversaire = message.mentions.members.first() || message.guild.members.get(args[0]);
-    const xpMiser = args[1];
+    const xpMiser = parseInt(args[1]);
 
     if (adversaire.id == challenger) {
         message.reply("Tu peux pas te challenge toi meme aha.");
@@ -130,7 +130,7 @@ module.exports.run = async (client, message, args) => {
                                                     docChallenger.exec((err, doc) => {
                                                         if (err) console.log(err);
                             
-                                                        doc.XP = doc.XP + xpMiser;
+                                                        doc.XP += xpMiser;
                                                         doc.save()
 
                                                     })    
