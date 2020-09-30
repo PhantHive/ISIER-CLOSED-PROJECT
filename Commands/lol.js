43fa5063b6dde69b35192d198430d1e6a9f3fdb6
Thanatos <sunn@gmx.com>
2020-09-30 23:17:01 +0200

    lol command

--- Commands/lol.js
+++ Commands/lol.js
@@ -1,16 +1,27 @@
 const { Kayn } = require("kayn");
 
-const kayn = Kayn(process.env.RIOT_LOL_API_KEY);
+const kayn = Kayn(process.env.RIOT_LOL_API_KEY)({
+    region: REGIONS.NORTH_AMERICA,
+    locale: 'en_US',
+    debugOptions: {
+        isEnabled: true,
+        showKey: false,
+    },
+    requestOptions: {}, // Doesn't apply to DDragon requests
+    cacheOptions: {
+        cache: new LRUCache({ max: 5000 }),
+        timeToLives: {
+            useDefault: true, // Cache DDragon by default!
+        },
+    },
+})
 
 
 module.exports.run = async(client, message, args) => {
 
-    kayn.DDragon.Champion.list()
-        .callback(function(error, champions) {
-            message.channel.send(champions)
-        })
- 
-
+    const championsRotate = kayn.Champion.Rotation.list();
+    message.channel.send(championsRotate);
+    
 };
 
 module.exports.help = {
