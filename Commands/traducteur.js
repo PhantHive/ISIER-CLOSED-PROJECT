d061148315e87ed0c3324c3c96906a83ef8b5e91
Thanatos <sunn@gmx.com>
2020-10-01 21:48:32 +0200

    xp bot

--- Commands/traducteur.js
+++ Commands/traducteur.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const urban = require('relevant-urban');
 const translate = require('translate');
+const urban = require('relevant-urban');
 
-const urbanCommand = require('./definition.js');
 
 translate.engine = 'yandex';
 translate.key = process.env.TRANSLATE_KEY;
@@ -35,11 +35,30 @@ module.exports.run = async (client, message, args) => {
 
             switch (reaction.emoji.name) {
                 case '📖':
-                    message.channel.send(`i!def ${motTraduit}`).then(message => {
-                        message.delete(100)
+        
+                    let res = await urban(args.join(' ')).catch(e => {
+                        return message.channel.send('mot introuvable dans le dico');
                     });
+                    const definition = `***DEF:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`
+                    const deftofr = await translate(definition, 'fr')
 
+                    const embed = new Discord.RichEmbed()
+                        .setColor('ORANGE')
+                        .setTitle(`URBAN DICO | ${res.word}`)
+                        .setURL(res.urbanURL)
+                        .setDescription(deftofr)
+                        .addField('Auteur:', res.author, true);
+
+
+                    if (res.tags.length > 0 && res.tags.join(', ').length < 1024) {
+
+                        embed.addField('Tags:', res.tags.join(', '), true)
+
+                    }
+
+                    message.channel.send(embed);
                     break;
+     
 
                 case '❌':
                     message.channel.send("Au plaisir de t\'avoir aider :)");
