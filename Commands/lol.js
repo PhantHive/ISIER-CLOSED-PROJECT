0aa58d04e9f66e6ddf9391414f47b47b46c20b3e
Thanatos <sunn@gmx.com>
2020-10-01 23:57:32 +0200

    xp bot

--- Commands/lol.js
+++ Commands/lol.js
@@ -25,7 +25,7 @@ const kayn = Kayn(process.env.RIOT_LOL_API_KEY)({
 
 module.exports.run = async(client, message, args) => {
 
-    const championsRotate = kayn.DDragon.Champion.getDataByIdWithParentAsId("Lux");
+    const championsRotate = kayn.DDragon.Champion.listFull()
     console.log(championsRotate);
     
     
