0d5265a27c077da52ff429d759a45984172e96fc
Thanatos <sunn@gmx.com>
2020-11-04 21:07:04 +0100

    Mini projet command

--- Events/ready.js
+++ Events/ready.js
@@ -3,6 +3,6 @@ module.exports = async(client) => {
 
     client.user.setActivity(`${622617588010188830} NETFLIX I M BORED`,{type: "watching"});
 
-    client.user.setActivity(`NETFLIX I M BORED` ,{type: "watching"});
+    client.user.setActivity(`EN MAINTENANCE` ,{type: "watching"});
 
 };
