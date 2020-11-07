1b6219d22a3f36768db0c60f91577830203543e8
Thanatos <sunn@gmx.com>
2020-11-07 02:45:08 +0100

    mini projet envoi

--- Events/message.js
+++ Events/message.js
@@ -117,7 +117,7 @@ module.exports = async(client, message) => {
 
     var mp = String(message.content);
 
-    if (mp.search(/mp ph111/i  || /miniprojet ph111/i   || /projet ressort/i   || /miniprojet ressort/i   || /mp ressort/i   || /mini projet resort/i  )) {
+    if (mp.search(/mp ph111/i  || /miniprojet ph111/i   || /projet ressort/i   || /miniprojet ressort/i   || /mp ressort/i   || /mini projet resort/i  ) !== -1) {
 
         message.channel.startTyping()
         message.reply("je t envoi ca de suite")
