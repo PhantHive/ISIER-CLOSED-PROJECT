d2ea8a437a375d01311f492483eff17e3c2a97dd
Thanatos <sunn@gmx.com>
2020-12-21 23:30:31 +0100

    fiche chemin mis a jour

--- Commands/coordoFiche.js
+++ Commands/coordoFiche.js
@@ -4,7 +4,7 @@ const { join } = require("path");
 module.exports.run = async(client, message) => {
 
     message.channel.startTyping();
-    message.channel.send('✍️').then(m => m.delete(2000))
+    message.channel.send('✍️').then(m => m.delete(1500))
     message.channel.send({file: join(__dirname, "..", "aero1Sources", "fiche", "physique", "CoordoPh111.pdf")})
     message.channel.stopTyping();
 
