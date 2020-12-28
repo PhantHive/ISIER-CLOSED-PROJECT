07500d7e7a62b0a05635e7e78736e41ec0091f0c
Thanatos <sunn@gmx.com>
2020-12-28 01:53:07 +0100

    objection, rang updated to V12

--- Commands/miscellaneous/objection.js
+++ Commands/miscellaneous/objection.js
@@ -1,29 +1,34 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (client, message, args) => {
-    const rando_imgs = [
-        './objectionImage/objection1.gif',
-        './objectionImage/objection2.jpg',
-        './objectionImage/objection3.png',
-        './objectionImage/objection4.gif',
-        './objectionImage/objection5.jpg',
-        './objectionImage/objection6.jpg',
-        './objectionImage/objection7.gif',
-        './objectionImage/objection8.gif',
-        './objectionImage/objection9.gif',
-        './objectionImage/objection10.gif',
-        './objectionImage/objection11.gif',
-        './objectionImage/objection12.gif',
-        './objectionImage/objection13.jpg'
-    ];
+module.exports =  {
+    name:"objection",
+    aliases: ["wrong", "youwrong", "non", "faux"],
+    category:"miscellaneous",
+    description:"send an objection pic",
+    usage:"objection",
+    run: async(client,message) => {
+        const rando_imgs = [
+            './objectionImage/objection1.gif',
+            './objectionImage/objection2.jpg',
+            './objectionImage/objection3.png',
+            './objectionImage/objection4.gif',
+            './objectionImage/objection5.jpg',
+            './objectionImage/objection6.jpg',
+            './objectionImage/objection7.gif',
+            './objectionImage/objection8.gif',
+            './objectionImage/objection9.gif',
+            './objectionImage/objection10.gif',
+            './objectionImage/objection11.gif',
+            './objectionImage/objection12.gif',
+            './objectionImage/objection13.jpg'
+        ];
 
 
-    message.channel.send({
-        file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
-    });
+        await message.channel.send({
+            files: [
+                rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+            ]
+        });
+    }
 };
 
-
-module.exports.help = {
-    name:"objection"
-};
