4b13cf1634580cdcb9c82f34a95c52f852930498
Thanatos <sunn@gmx.com>
2020-12-28 02:20:30 +0100

    poll updated to V12

--- Commands/miscellaneous/sondage.js
+++ Commands/miscellaneous/sondage.js
@@ -1,93 +1,97 @@
-const Discord = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 
-module.exports.run = async (client, message, args) => {
+module.exports =  {
 
-    const infoPoll = message.content.match(/".+?"/g).map(str => str.replace(/"/g, ''));
+    name:"sondage",
+    aliases: ["poll"],
+    category:"miscellaneous",
+    description:"make a poll",
+    usage:"sondage",
+    run: async(client, message, args) => {
 
-    if(infoPoll.length > 1) {
+        const infoPoll = message.content.match(/".+?"/g).map(str => str.replace(/"/g, ''));
 
-        if (infoPoll.length === 3) {
-            let sondageEmbed = new Discord.MessageEmbed()
-                .setTitle("SONDAGE- CHOIX MULTIPLES")
-                .setDescription(infoPoll[0])
-                .addField('choix A' , infoPoll[1])
-                .addField('choix B' , infoPoll[2])
-            
+        if(infoPoll.length > 1) {
 
-            let sondageMessage = await message.channel.send(sondageEmbed);
-                await sondageMessage.react('🇦');
-                await sondageMessage.react('🇧');
+            if (infoPoll.length === 3) {
+                let sondageEmbed = new MessageEmbed()
+                    .setTitle("SONDAGE- CHOIX MULTIPLES")
+                    .setDescription(infoPoll[0])
+                    .addField('choix A' , infoPoll[1])
+                    .addField('choix B' , infoPoll[2])
 
-        }
 
-        else if (infoPoll.length === 4) {
-            let sondageEmbed = new Discord.MessageEmbed()
-                .setTitle("SONDAGE- CHOIX MULTIPLES")
-                .setDescription(infoPoll[0])
-                .addField('choix A' , infoPoll[1])
-                .addField('choix B' , infoPoll[2])
-                .addField('choix C' , infoPoll[3])
-            
+                let sondageMessage = await message.channel.send(sondageEmbed);
+                    await sondageMessage.react('🇦');
+                    await sondageMessage.react('🇧');
 
-            let sondageMessage = await message.channel.send(sondageEmbed);
-                await sondageMessage.react('🇦');
-                await sondageMessage.react('🇧');
-                await sondageMessage.react('🇨');
-                
-        }
+            }
 
-        else if (infoPoll.length === 5) {
+            else if (infoPoll.length === 4) {
+                let sondageEmbed = new MessageEmbed()
+                    .setTitle("SONDAGE- CHOIX MULTIPLES")
+                    .setDescription(infoPoll[0])
+                    .addField('choix A' , infoPoll[1])
+                    .addField('choix B' , infoPoll[2])
+                    .addField('choix C' , infoPoll[3])
 
-            let sondageEmbed = new Discord.MessageEmbed()
-                .setTitle("SONDAGE- CHOIX MULTIPLES")
-                .setDescription(infoPoll[0])
-                .addField('choix A' , infoPoll[1])
-                .addField('choix B' , infoPoll[2])
-                .addField('choix C' , infoPoll[3])
-                .addField('choix D' , infoPoll[4])
-            
 
-            let sondageMessage = await message.channel.send(sondageEmbed);
-                await sondageMessage.react('🇦');
-                await sondageMessage.react('🇧');
-                await sondageMessage.react('🇨');
-                await sondageMessage.react('🇩');
+                let sondageMessage = await message.channel.send(sondageEmbed);
+                    await sondageMessage.react('🇦');
+                    await sondageMessage.react('🇧');
+                    await sondageMessage.react('🇨');
+
+            }
+
+            else if (infoPoll.length === 5) {
+
+                let sondageEmbed = new MessageEmbed()
+                    .setTitle("SONDAGE- CHOIX MULTIPLES")
+                    .setDescription(infoPoll[0])
+                    .addField('choix A' , infoPoll[1])
+                    .addField('choix B' , infoPoll[2])
+                    .addField('choix C' , infoPoll[3])
+                    .addField('choix D' , infoPoll[4])
+
+
+                let sondageMessage = await message.channel.send(sondageEmbed);
+                    await sondageMessage.react('🇦');
+                    await sondageMessage.react('🇧');
+                    await sondageMessage.react('🇨');
+                    await sondageMessage.react('🇩');
+            }
+
+            else {
+                message.reply("tu ne peux pas faire un sondage avec une seul option LoL XD ptdrr.").then(m => m.delete(4000));
+            }
+
         }
 
         else {
-            message.reply("tu ne peux pas faire un sondage avec une seul option LoL XD ptdrr.").then(m => m.delete(4000));
-        }                                                                  
 
-    }
 
-    else {
+            let sondageEmbed = new MessageEmbed()
+                .setTitle("SONDAGE")
+                .setDescription(args[0]);
 
-        
-        let sondageEmbed = new Discord.MessageEmbed()
-            .setTitle("SONDAGE")
-            .setDescription(args[0]);
+            let sondageMessage = await message.channel.send(sondageEmbed);
+            await sondageMessage.react('👍');
+            await sondageMessage.react('👎');
 
-        let sondageMessage = await message.channel.send(sondageEmbed);
-        await sondageMessage.react('👍');
-        await sondageMessage.react('👎');
+            const filter = (reaction) => reaction.emoji.name === '👍' || reaction.emoji.name === '👎'
+            const results = await sondageMessage.awaitReactions(filter, {
+                time: 864000
+            });
 
-        const filter = (reaction) => reaction.emoji.name === '👍' || reaction.emoji.name === '👎'
-        const results = await sondageMessage.awaitReactions(filter, {
-            time: 864000
-        });
+            let resultsEmbed = new MessageEmbed()
+                .setTitle("Resultat du sondage:")
+                .setDescription(`Resultats: ${args[0].join(" ")}`)
+                .addField("👍: ", ` ${results.get('👍').count-1}`)
+                .addField("👍: ", ` ${results.get('👎').count - 1}`)
+            client.channels.get('613749495716642818').send(resultsEmbed);
 
-        let resultsEmbed = new Discord.MessageEmbed()
-            .setTitle("Resultat du sondage:")
-            .setDescription(`Resultats: ${args[0].join(" ")}`)
-            .addField("👍: ", ` ${results.get('👍').count-1}`)
-            .addField("👍: ", ` ${results.get('👎').count - 1}`)
-        client.channels.get('613749495716642818').send(resultsEmbed);
-
+        }
     }
     
 };
 
-
-module.exports.help = {
-    name: "sondage"
-};
