7ccf0a509e8de05cbf6a9635a76eaa46e134d60b
Thanatos <sunn@gmx.com>
2020-12-28 14:51:12 +0100

    clear.js ==> updated to V12

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js');
 
-module.exports.run = async(client, message, args) => {
+module.exports =  {
+    name:"clear",
+    aliases: ["swallow", "prune", "purge", "clean"],
+    category:"moderation",
+    description:"clear messages",
+    timeout: 5500,
+    usage:"clear",
+    run: async(client,message, args) => {
+        if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.channel.send("tu n'as pas la permission").catch(console.log);
+        if (!args[0]) return message.channel.send("indiquer un nombre de messages");
 
-    if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.channel.send("tu n'as pas la permission").catch(console.log);
-    if(!args[0]) return message.channel.send("indiquer un nombre de messages");
+        if (isNaN(args[0])) return message.channel.send("indiquer un nombre");
 
-    if (isNaN(args[0])) return message.channel.send("indiquer un nombre");
-
-    message.channel.bulkDelete(args[0]);
+        await message.channel.bulkDelete(args[0]);
+    }
 
 };
 
-module.exports.help = {
-    name: "clear"
-};
