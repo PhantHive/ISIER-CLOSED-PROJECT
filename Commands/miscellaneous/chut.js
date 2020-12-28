1b61549130f90b5683080ecec0bbd69ccf929a3c
Thanatos <sunn@gmx.com>
2020-12-28 01:36:17 +0100

    chut command update to v12

--- Commands/miscellaneous/chut.js
+++ Commands/miscellaneous/chut.js
@@ -1,30 +1,30 @@
-const Discord = require('discord.js');
+module.exports = {
+    name:"chut",
+    aliases: ["tg", "shut", "shutup", "ftg"],
+    category:"miscellaneous",
+    description:"send chut pic",
+    usage:"chut",
+    run: async(client,message) => {
+        const rando_imgs = [
+            './chut/chut1.jpg',
+            './chut/chut2.jpg',
+            './chut/chut3.jpg',
+            './chut/chut4.jpg',
+            './chut/chut5.jpg',
+            './chut/chut6.jpg',
+            './chut/chut7.jpg',
+            './chut/chut8.png',
+            './chut/chut9.jpg',
+            './chut/chut10.jpg',
+            './chut/chut11.jpg',
+            './objectionImage/objection12.gif',
+            './chut/chut12.jpg'
+        ];
 
-module.exports.run = async (client, message, args) => {
-    const rando_imgs = [
-        './chut/chut1.jpg',
-        './chut/chut2.jpg',
-        './chut/chut3.jpg',
-        './chut/chut4.jpg',
-        './chut/chut5.jpg',
-        './chut/chut6.jpg',
-        './chut/chut7.jpg',
-        './chut/chut8.png',
-        './chut/chut9.jpg',
-        './chut/chut10.jpg',
-        './chut/chut11.jpg',
-        './objectionImage/objection12.gif',
-        './chut/chut12.jpg'
-    ];
 
-
-    message.channel.send({
-        file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
-    });
+        message.channel.send({
+            file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+        });
+    }
 };
 
-
-
-module.exports.help = {
-    name:"chut"
-};
