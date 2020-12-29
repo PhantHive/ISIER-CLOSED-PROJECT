196a6093866cf07a255f0bae2873463f3968e888
Thanatos <sunn@gmx.com>
2020-12-29 02:11:13 +0100

    message.js trying to update to V12

--- events/guild/message.js
+++ events/guild/message.js
@@ -233,7 +233,7 @@ module.exports = (client, message) => {
     }
 
 
-    client.on("message", (message) => {
+    client.on("message", async (message) => {
         //=============
         var sponso = String(message.content);
         const rand_answer = [
