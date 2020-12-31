07d359e73b059833eee0b331cfe6c1cb14f658e9
Thanatos <sunn@gmx.com>
2020-12-31 19:36:27 +0100

    Ipsa-Bot commands from v11 --> v12

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,13 +1,13 @@
 module.exports = (client) => {
     const memberSize = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
-    const activity = ["`${memberSize} ipsaliens :)`",
-                    "---> !!aide"]
+    const activity = [`${memberSize} ipsaliens :)`,
+                    "---> !!aide",
+                    "version V12"]
 
     console.log(`${client.user.username} connected`)
 
     setInterval(() => {
         const index = Math.floor(Math.random() * (activity.length - 1));
-        console.log(index);
         client.user.setActivity(activity[index], {type: 'WATCHING', name:"To you"}); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
 
