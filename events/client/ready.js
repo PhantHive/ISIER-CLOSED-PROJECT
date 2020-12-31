302ecfe41d633a8495be0389b4697b46dc30093e
Thanatos <sunn@gmx.com>
2020-12-31 19:33:58 +0100

    Ipsa-Bot commands from v11 --> v12

--- events/client/ready.js
+++ events/client/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
     console.log(`${client.user.username} connected`)
 
     setInterval(() => {
-        const index = Math.floor(Math.random() * (activity.length - 1) + 1);
+        const index = Math.floor(Math.random() * (activity.length - 1));
         console.log(index);
         client.user.setActivity(activity[index], {type: 'WATCHING', name:"To you"}); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
