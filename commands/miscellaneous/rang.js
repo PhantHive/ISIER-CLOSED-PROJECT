9591cee744db81af99cdf9651645103903bea1b7
Thanatos <sunn@gmx.com>
2021-04-09 20:39:59 +0200

    Hexagon canvas try?

--- commands/miscellaneous/rang.js
+++ commands/miscellaneous/rang.js
@@ -179,10 +179,10 @@ module.exports =  {
                                 //profil circle
 
 
-                                ctx.moveTo(170 + 200 * Math.cos(0), 180 + 200 * Math.sin(0));
+                                ctx.moveTo(70 + 200 * Math.cos(0), 180 + 200 * Math.sin(0));
 
                                 for (let side = 0; side < 7; side++) {
-                                    ctx.lineTo(80 + 150 * Math.cos(side * 2 * Math.PI / 6), 80 + 150 * Math.sin(side * 2 * Math.PI / 6));
+                                    ctx.lineTo(180 + 150 * Math.cos(side * 2 * Math.PI / 6), 180 + 150 * Math.sin(side * 2 * Math.PI / 6));
                                 }
 
                                 ctx.lineWidth = 6;
@@ -191,7 +191,7 @@ module.exports =  {
                                 ctx.closePath();
                                 ctx.clip();
                                 const avatar = await loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-                                ctx.drawImage(avatar, 40, 60, 250, 250);
+                                ctx.drawImage(avatar, 80, 100, 250, 250);
 
                                 /*
 
