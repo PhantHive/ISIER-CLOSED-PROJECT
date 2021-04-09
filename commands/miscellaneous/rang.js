f0ca34b1c814ee7aa60a0bb64c251b7dc228261c
Thanatos <sunn@gmx.com>
2021-04-09 22:38:04 +0200

    rank card fix

--- commands/miscellaneous/rang.js
+++ commands/miscellaneous/rang.js
@@ -191,7 +191,7 @@ module.exports =  {
                                 ctx.closePath();
                                 ctx.clip();
                                 const avatar = await loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-                                ctx.drawImage(avatar, 0, 37, 200, 200);
+                                ctx.drawImage(avatar, 10, 37, 200, 200);
 
                                 /*
 
