490ce2552bf385dce85f7de4f7cf6ca6f8ce169a
Thanatos <sunn@gmx.com>
2021-04-09 22:22:10 +0200

    rank card fix

--- commands/miscellaneous/rang.js
+++ commands/miscellaneous/rang.js
@@ -179,10 +179,10 @@ module.exports =  {
                                 //profil circle
 
 
-                                ctx.moveTo(90 + 100 * Math.cos(0), 120 + 100 * Math.sin(0));
+                                ctx.moveTo(100 + 100 * Math.cos(0), 120 + 100 * Math.sin(0));
 
                                 for (let side = 0; side < 7; side++) {
-                                    ctx.lineTo(90 + 100 * Math.cos(side * 2 * Math.PI / 6), 120 + 100 * Math.sin(side * 2 * Math.PI / 6));
+                                    ctx.lineTo(100 + 100 * Math.cos(side * 2 * Math.PI / 6), 120 + 100 * Math.sin(side * 2 * Math.PI / 6));
                                 }
 
                                 ctx.lineWidth = 6;
@@ -191,7 +191,7 @@ module.exports =  {
                                 ctx.closePath();
                                 ctx.clip();
                                 const avatar = await loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-                                ctx.drawImage(avatar, 40, 40, 200, 200);
+                                ctx.drawImage(avatar, 10, 37, 200, 200);
 
                                 /*
 
