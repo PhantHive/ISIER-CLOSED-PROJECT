c491a80e9d11040812076d21e2af28763aa703ea
Thanatos <sunn@gmx.com>
2021-04-10 16:58:44 +0200

    Canvas modif

--- commands/miscellaneous/rang.js
+++ commands/miscellaneous/rang.js
@@ -142,12 +142,13 @@ module.exports =  {
 
                                 //bar rempli
                                 ctx.beginPath();
-                                ctx.lineWidth = 1;
                                 ctx.fillStyle = gradient;
-                                ctx.strokeStyle = gradient;
-                                ctx.globalAlpha = 0.7;
+                                //ctx.globalAlpha = 0.7;
                                 ctx.moveTo(200, 100)
-                                ctx.fillRect(200, 100, percentage, 15)
+                                ctx.quadraticCurveTo(190, 110, 200, 120) //left arc (pi/2 : - pi/2)
+                                ctx.lineTo(20 + percentage, 120)
+                                ctx.quadraticCurveTo(210 + percentage, 110, 200 + percentage, 100) //right arc (-pi/2 : pi/2)
+                                ctx.lineTo(200, 100)
                                 ctx.stroke();
                                 ctx.fill();
                                 ctx.closePath();
