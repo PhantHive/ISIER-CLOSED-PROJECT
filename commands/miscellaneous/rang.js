5b4e1314afd01b19efca0a3cc555f4a32a14c683
Thanatos <sunn@gmx.com>
2021-04-10 17:14:23 +0200

    Canvas modif

--- commands/miscellaneous/rang.js
+++ commands/miscellaneous/rang.js
@@ -145,10 +145,14 @@ module.exports =  {
                                 ctx.fillStyle = gradient;
                                 //ctx.globalAlpha = 0.7;
                                 ctx.moveTo(200, 100)
-                                ctx.quadraticCurveTo(190, 110, 200, 120) //left arc (pi/2 : - pi/2)
-                                ctx.lineTo(200 + percentage, 120)
-                                ctx.quadraticCurveTo(210 + percentage, 110, 200 + percentage, 100) //right arc (-pi/2 : pi/2)
-                                ctx.lineTo(200, 100)
+
+
+                                ctx.arc(200, 100, 5, 1.5 * Math.PI, 0.5 * Math.PI, true);
+                                ctx.fill();
+                                ctx.fillRect(200, 100, 200 + percentage, 20);
+                                ctx.arc(200 + percentage, 100, 5, 1.5 * Math.PI, 0.5 * Math.PI, false);
+                                ctx.fill();
+
                                 ctx.stroke();
                                 ctx.fill();
                                 ctx.closePath();
