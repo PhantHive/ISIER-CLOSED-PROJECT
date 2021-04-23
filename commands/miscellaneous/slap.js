16d63db41208bc2a53c95db200189dc15bd6a0bb
Thanatos <sunn@gmx.com>
2021-04-23 20:09:57 +0200

    modif rank card

--- commands/miscellaneous/slap.js
+++ commands/miscellaneous/slap.js
@@ -11,8 +11,8 @@ module.exports = {
     usage: "slap",
     run: async (client, message, args) => {
 
-        let url = `https://api.tenor.com/v1/search?q=animeslap&key=NT23U13IZ0AH&limit=10&MediaFilter=tinygif`
-        let response = await fetch(url);
+        let tenorUrl = `https://api.tenor.com/v1/search?q=animeslap&key=NT23U13IZ0AH&limit=10&MediaFilter=tinygif`
+        let response = await fetch(tenorUrl);
         let result = await response.json();
         console.log(result)
         let index = Math.floor(Math.random() * result.results.length);
@@ -20,7 +20,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setColor('ORANGE')
             .setTitle(`I SLAP U |`)
-            .setImage(result.results[index]["tinygif"]["url"])
+            .setImage(result.results[index].itemurl)
             .setDescription(`<@${message.author.id}> slap ${args[0]}`);
         await message.channel.send(embed);
 
