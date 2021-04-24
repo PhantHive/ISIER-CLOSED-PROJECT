e12d9bcb8a2062bf90713a42eca83d2b8e537290
Thanatos <sunn@gmx.com>
2021-04-24 11:49:07 +0200

    add owo, uwu, hug and slap command, fixed rank card form problem

---
+++ commands/miscellaneous/owo.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require("node-fetch");
+
+module.exports = {
+
+    name: "owo",
+    aliases: ["owo"],
+    timeout: 20000,
+    category: "miscellaneous",
+    description: "owo",
+    usage: "owo",
+    run: async (client, message, args) => {
+
+
+
+        let tenorUrl = `https://api.tenor.com/v1/search?q=owo&key=NT23U13IZ0AH&limit=20`
+        let response = await fetch(tenorUrl);
+        let jsonResults = await response.json()
+        let index = Math.floor(Math.random() * jsonResults.results.length);
+
+        await message.channel.send({ files: [jsonResults.results[index].media[0].mediumgif.url] });
+
+
+
+    }
+
+}
