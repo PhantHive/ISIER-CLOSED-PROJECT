5a29fa811add727d13334a0c522340c7d1663bbd
Thanatos <sunn@gmx.com>
2021-09-04 23:54:06 +0200

    verificationd e mail en privé

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -2,7 +2,7 @@
 
 module.exports = (client, member, message) => {
 
-
+    console.log("test")
     member.createDM().then(channel => {
         channel.send("Bonjour je suis chargé de te vérifier!\n " +
             "envoie moi **ready** pour etre vérifier!")
