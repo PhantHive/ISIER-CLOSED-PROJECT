4ecc217218852dea690a7c0d3dd28a9d6377399a
Thanatos <sunn@gmx.com>
2021-09-05 02:50:22 +0200

    Mail verification system with DB (noSQL)

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -5,7 +5,8 @@ module.exports = (client, member, message) => {
     console.log("test")
     member.createDM().then(channel => {
         channel.send("Bonjour je suis chargé de te vérifier!\n " +
-            "Envoie ton mail sous format: prenom.nom@ipsa.fr")
+            "Envoie ton mail sous format: **prenom.nom@ipsa.fr**\n" +
+            "(environs 5 secondes pour traiter la demande)")
 
     }).catch(err => console.log(err))
 
