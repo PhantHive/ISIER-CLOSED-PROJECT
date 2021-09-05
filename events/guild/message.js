6f703bbd6d6ec77367f13a72aaf76a3f885e261a
Thanatos <sunn@gmx.com>
2021-09-05 13:22:25 +0200

    adding role on join

--- events/guild/message.js
+++ events/guild/message.js
@@ -950,12 +950,12 @@ module.exports = (client, message) => {
                                                             if (guild.members.fetch(user)) {
 
                                                                 try {
-                                                                    role = message.guild.roles.cache.find(r => r.name === "IPSAlien");
+                                                                    role = message.member.guild.roles.cache.find(r => r.name === "IPSAlien");
                                                                 } catch (error) {
                                                                 }
 
                                                                 try {
-                                                                    oldrole = message.guild.roles.cache.find(r => r.name === "Invité");
+                                                                    oldrole = message.member.guild.roles.cache.find(r => r.name === "Invité");
                                                                 } catch (error) {
                                                                 }
 
