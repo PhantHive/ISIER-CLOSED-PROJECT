2f1f2cf9e6186d2ff040e5937e23880fe4504980
Thanatos <sunn@gmx.com>
2021-09-10 18:42:48 +0200

    update coordo sheets path

--- commands/studenthelp/coordoFiche.js
+++ commands/studenthelp/coordoFiche.js
@@ -11,7 +11,7 @@ module.exports =  {
     run: async(client,message) => {
         message.channel.startTyping();
         message.channel.send('✍️').then(m => m.delete(1000))
-        message.channel.send({files: [join(__dirname, "../ressources/aero1Sources/fiche/physique/CoordoPh111.pdf")]})
+        message.channel.send({files: [join(__dirname, "../../ressources/aero1Sources/fiche/physique/CoordoPh111.pdf")]})
         message.channel.stopTyping();
     }
 
